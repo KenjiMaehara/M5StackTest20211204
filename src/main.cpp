ce476e9cedaaa1c1ab2713f5940f0efbc61c098e
techhouse_150908 <kmaehara@mel.co.jp>
2021-12-06 06:43:24 +0900

    ファイル追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <M5Core2.h>
+#include "menu/menu.h"
 
 
 
@@ -7,6 +8,14 @@ void setup() {
   M5.begin(true, true, true, true);
   M5.Lcd.setTextSize(3);
   M5.Lcd.print("Hello World");
+
+
+  SD_read_forSSID();
+
+  ssid = ssidTemp.c_str();
+  password = passTemp.c_str();
+	printf("AP_SSID:  %s\n",ssid);
+	printf("AP_PASSWORD:  %s\n",password);
 }
 
 void loop() {
